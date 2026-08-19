0569add509e248b9fe3bf89aca0ce2806cb4ee39
upendrakda <upendrakda123@gmail.com>
2026-08-19 21:11:56 +0545

    update program to implement conept of tokenization

--- NLP/04_tokenization.py
+++ NLP/04_tokenization.py
@@ -1,16 +1,19 @@
 # Write a program to implement the concept of Tokenization
 
 import os
+import nltk
+from nltk.tokenize import word_tokenize
 
 os.system("cls")
 
 # Input sentence
 sentence = input("Enter a sentence: ")
 
-# Word tokenization
-words = sentence.split()
+# Tokenization
+tokens = word_tokenize(sentence)
 
 # Display tokens
 print("\nTokens:")
-for i, word in enumerate(words, 1):
-    print(f"Token {i}: {word}")
+
+for i, token in enumerate(tokens, 1):
+    print(f"Token {i}: {token}")

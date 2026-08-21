918e6cc6318443eb7efa392f8f3364e7bd1224cb
upendrakda <upendrakda123@gmail.com>
2026-08-21 17:28:58 +0545

    add prolog lab 7

---
+++ Prolog/lab07.pl
@@ -0,0 +1,27 @@
+calculate :-
+    write('Enter first number: '),
+    read(A),
+
+    write('Enter second number: '),
+    read(B),
+
+    Sum is A + B,
+    Difference is A - B,
+    Product is A * B,
+    Division is A / B,
+
+    write('Sum = '),
+    write(Sum),
+    nl,
+
+    write('Difference = '),
+    write(Difference),
+    nl,
+
+    write('Product = '),
+    write(Product),
+    nl,
+
+    write('Division = '),
+    write(Division),
+    nl.
